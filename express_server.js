b18e4c6e1559f0ecc112beaaa8545a1d675041ca
Rebecca Haliburton <haliburtonre@gmail.com>
2017-06-07 19:41:09 +0000

    Added delete and update functionality

--- express_server.js
+++ express_server.js
@@ -25,9 +25,18 @@ function displayError(message) {
   alert(message);
 };
 
-app.get("/", (req, res) => {
-  res.render("urls_new");
+
+app.get('/', (req, res) => {
+  res.redirect('/urls');
 });
+
+app.get("/urls/new", (req, res) => {
+  var templateVars = {
+    error: req.query.error
+  };
+  res.render("urls_new", templateVars);
+});
+
 
 app.get("/urls", (req, res) => {
   let templateVars = {
@@ -38,19 +47,33 @@ app.get("/urls", (req, res) => {
 
 app.post("/urls", (req, res) => {
   if (req.body['longURL'].includes('http://')) {
-    const newURL = generateRandomString();
-    urlDatabase[newURL] = req.body['longURL'];
-    res.redirect(`/urls/${newURL}`);
-    } else {
-    res.render("urls_newError");
+    const newID = generateRandomString();
+    urlDatabase[newID] = req.body['longURL'];
+    res.redirect(`/urls/${newID}`);
+  } else {
+    res.redirect('/urls/new?error=InvalidPath');
   }
 });
 
+app.post("/urls/:id/delete", (req, res) => {
+    delete urlDatabase[req.params.id];
+    res.redirect("/urls");
+});
+
+app.post("/urls/:id", (req, res) => {
+    if (req.body['longURL'].includes('http://')) {
+    urlDatabase[req.params.id] = req.body['longURL'];
+    res.redirect(`/urls/${req.params.id}`);
+  } else {
+    res.redirect(`/urls/${req.params.id}?error=InvalidPath`);
+  }
+});
 
 app.get("/urls/:id", (req, res) => {
-  let templateVars = {
+    let templateVars = {
     urls: urlDatabase[req.params.id],
-    shortURL: req.params.id
+    shortURL: req.params.id,
+    error: req.query.error
   };
   res.render("urls_show", templateVars);
 });
